289c0910c841d671aa1003f7bfbd45c71e13f2b2
Simon Willison <swillison@gmail.com>
2024-03-15 17:30:01 -0700

    Temporary file cleanup, refs #5
    We now delete the file after it has been downloaded
    
    On startup and every hour we delete any stale temporary files

--- datasette_export_database/__init__.py
+++ datasette_export_database/__init__.py
@@ -8,6 +8,10 @@ import pathlib
 import secrets
 import shutil
 import tempfile
+import time
+
+TMP_PREFIX = "datasette-export-database-"
+tmp_dir = tempfile.gettempdir()
 
 
 @hookimpl
@@ -41,6 +45,31 @@ def permission_allowed(action, actor):
 
 
 @hookimpl
+def startup(datasette):
+    datasette._datasette_export_database_cleanup_task = asyncio.create_task(
+        cleanup_task()
+    )
+
+
+def cleanup_task_sync():
+    path = pathlib.Path(tmp_dir)
+    for file in path.glob(TMP_PREFIX + "*"):
+        # We use modified not created time because our tests can
+        # fake the modified time but harder to create created time
+        age_in_seconds = time.time() - file.stat().st_mtime
+        if age_in_seconds > 3600:
+            file.unlink()
+
+
+async def cleanup_task():
+    # Once an hour scans /tmp for TMP_PREFIX files that were created more than an hour ago
+    while True:
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, cleanup_task_sync)
+        await asyncio.sleep(60 * 60)
+
+
+@hookimpl
 async def export_database(datasette, request, send):
     database = request.url_vars["database"]
     db = datasette.get_database(database)
@@ -65,15 +94,21 @@ async def export_database(datasette, request, send):
 
     # Is there enough space in /tmp ?
     db_size_bytes = pathlib.Path(db_path).stat().st_size
-    free_tmp_bytes = shutil.disk_usage("/tmp")[2]
+    free_tmp_bytes = shutil.disk_usage(tmp_dir)[2]
     if free_tmp_bytes < db_size_bytes:
         return Response.text(
-            "Not enough space in /tmp to export this database", status=403
+            "Not enough space in {} to export this database".format(tmp_dir), status=403
         )
 
     # Generate a unique filename for the new SQLite database in the /tmp directory
     with tempfile.NamedTemporaryFile(
-        prefix=database, suffix=".db", delete=False, dir="/tmp"
+        prefix="{}{}".format(
+            TMP_PREFIX,
+            database,
+        ),
+        suffix=".db",
+        delete=False,
+        dir=tmp_dir,
     ) as tmp_file:
         new_db_path = tmp_file.name
 
@@ -99,6 +134,8 @@ async def export_database(datasette, request, send):
             content_type="application/octet-stream",
             chunk_size=4096,
         )
+        # Now delete the temp file
+        pathlib.Path(new_db_path).unlink()
     else:
         # Handle any errors that occurred during the subprocess execution
         error_message = stderr.decode().strip()
--- tests/test_export_database.py
+++ tests/test_export_database.py
@@ -1,24 +1,32 @@
+import asyncio
 from datasette.app import Datasette
+import httpx
+import os
+import pathlib
 import pytest
+import tempfile
+import time
 import shutil
 import sqlite3
 
 
-@pytest.fixture
-def db_path(tmpdir):
+@pytest.fixture(scope="session")
+def db_path(tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp("dbs")
     path = str(tmpdir / "data.db")
     conn = sqlite3.connect(path)
     with conn:
-        conn.execute("create table big (id integer primary key, string text)")
-        conn.executemany(
-            "insert into big (string) values (?)",
-            (["12349871243987213948 " * 100] for i in range(1001)),
-        )
+        for i in range(10):
+            conn.execute(f"create table big{i} (id integer primary key, string text)")
+            conn.executemany(
+                f"insert into big{i} (string) values (?)",
+                (["12349871243987213948 " * 100] for i in range(1001)),
+            )
     return path
 
 
 @pytest.mark.asyncio
-async def test_permissions(db_path, tmpdir):
+async def test_permissions_and_test_fetch(db_path, tmpdir):
     datasette = Datasette([db_path])
     anon_response1 = await datasette.client.get("/data/-/export-database")
     assert anon_response1.status_code == 403
@@ -31,6 +39,17 @@ async def test_permissions(db_path, tmpdir):
     assert "/export-database" in root_response1.text
     # Now find the signature
     signature = root_response1.text.split("/export-database?s=")[1].split('"')[0]
+
+    # Check we don't leave stale files at the end:
+    tmp = pathlib.Path(tempfile.gettempdir())
+
+    def prefixed_files():
+        return {
+            str(filepath) for filepath in tmp.glob("datasette-export-database-*.db")
+        }
+
+    before_files = prefixed_files()
+
     root_response = await datasette.client.get(
         "/data/-/export-database?s=" + signature,
         cookies=cookies,
@@ -39,13 +58,18 @@ async def test_permissions(db_path, tmpdir):
     content_disposition = root_response.headers["content-disposition"]
     assert content_disposition.startswith('attachment; filename="data-')
     assert content_disposition.endswith('.db"')
+
     # Write that out to disk so we can query it
     restored = str(tmpdir / "restored.db")
     with open(restored, "wb") as fp:
         for b in root_response.iter_bytes():
             fp.write(b)
     conn = sqlite3.connect(restored)
-    assert conn.execute("select count(*) from big").fetchone()[0] == 1001
+    assert conn.execute("select count(*) from big1").fetchone()[0] == 1001
+
+    # Should not have left any stale files in /tmp
+    after_files = prefixed_files() - before_files
+    assert not after_files
 
 
 @pytest.mark.asyncio
@@ -63,7 +87,32 @@ async def test_no_space(db_path, tmpdir, monkeypatch):
         cookies=cookies,
     )
     assert root_response.status_code == 403
-    assert root_response.text == "Not enough space in /tmp to export this database"
+    assert (
+        root_response.text
+        == "Not enough space in {} to export this database".format(
+            tempfile.gettempdir()
+        )
+    )
+
+
+@pytest.mark.asyncio
+async def test_cleans_up_stale_tmp_files_on_startup(db_path):
+    tmp = pathlib.Path(tempfile.gettempdir())
+    # Create two tmp files with fake last-modified dates
+    # one two hours ago, one two minutes ago
+    two_hours_ago = tmp / "datasette-export-database-two-hours-ago.db"
+    two_hours_ago.write_bytes(b"hello")
+    os.utime(str(two_hours_ago), times=(time.time() - 2 * 3600, time.time() - 2 * 3600))
+    two_minutes_ago = tmp / "datasette-export-database-two-minutes-ago.db"
+    two_minutes_ago.write_bytes(b"hello")
+    os.utime(str(two_minutes_ago), times=(time.time() - 2 * 60, time.time() - 2 * 60))
+    assert two_hours_ago.exists()
+    assert two_minutes_ago.exists()
+    # Starting Datasette should delete one but not the other
+    datasette = Datasette()
+    await datasette.client.get("/")
+    assert not two_hours_ago.exists()
+    assert two_minutes_ago.exists()
 
 
 @pytest.mark.asyncio
