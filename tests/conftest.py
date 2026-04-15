3cd2937c9469594ce05422bda0768a112a9fc4a9
Simon Willison <swillison@gmail.com>
2026-04-15 16:44:11 -0700

    Do not need to do this
    Refs https://github.com/datasette/datasette-export-database/pull/7/changes#r3090018083

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-from datasette.plugins import pm
-import datasette_export_database
-
-if not pm.is_registered(datasette_export_database):
-    pm.register(datasette_export_database, name="datasette_export_database")
